16705d171c9c958efc6c559a58c0bd00248f9480
Vinisha Hathiramani <vinishash@gmail.com>
2015-05-14 19:32:43 +0100

    logout heroku functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { :sessions => "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations"}
 
+  devise_scope :users do
+    get "sign_out", :to => "devise/sessions#destroy"
+  end
+
   resources :users do
     resources :reviews
   end
